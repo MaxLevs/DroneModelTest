9fb182216a27e7cbfb8c269828b5bf02d7916609
Maxim Liven <maxlevs@mail.ru>
2022-02-20 14:42:44 +0300

    Добавил заготовку сервиса симуляции

---
+++ DroneModelTest/SimulationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneModelTest
+{
+    /// <summary>
+    /// Сервис, занимающийся проведением симуляции
+    /// </summary>
+    public class SimulationService
+    {
+    }
+}
