cebdf459329479af8dac3de50842ff22c1589286
Maxim Liven <maxlevs@mail.ru>
2022-02-20 04:57:15 +0300

    xml-docs

--- DroneModelTest/Drone.cs
+++ DroneModelTest/Drone.cs
@@ -1,17 +1,30 @@
-﻿using System.Numerics;
+using System.Numerics;
 
 namespace DroneModelTest
 {
+    /// <summary>
+    /// Представляет дрона, проходящего некий маршрут
+    /// </summary>
     public class Drone
     {
+        /// <summary>
+        /// Уникальный ID дрона
+        /// </summary>
         public Guid Guid { get; init; } = Guid.NewGuid();
 
+        /// <summary>
+        /// Радиус, определяющий область, в которой находится дрон
+        /// </summary>
         public float Radius { get; init; }
+
 
         public Vector3 Position => MovementService.Position;
         public Vector3 Velocity => MovementService.Velocity;
         public Vector3 Acceleration => MovementService.Acceleration;
 
+        /// <summary>
+        /// Сервис, занимающийся перемещением дрона по заданному маршруту
+        /// </summary>
         MovementService MovementService { get; set; }
 
         public Drone(float radius, Vector3 startPosition, IEnumerable<DroneTrajectoryPartProperty> trajectoryProperties)
